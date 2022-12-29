39afcd8e0eb722f1059950980de5e34145825237
VaultreeAcademy <hey@vaultree.com>
2022-12-29 19:39:01 -0300

    structuring folder - initial commit

---
+++ starter-files/script.js
@@ -0,0 +1,9 @@
+/* 
+STEPS
+
+- Extract all selectors, create helper functions
+- Read through the APIs documentation and understand what needs to be included and create generic paramaters object
+- Regiter event listeners, fetch the data per the use's input
+- Output results to the UI (success and error)
+- Adjust UI states accordingly
+*/
