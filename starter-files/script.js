7ad7be1f94c50680c30670de6580b7e3d4e17b9c
VaultreeAcademy <hey@vaultree.com>
2022-12-29 21:26:31 -0300

    creating helper functions

--- starter-files/script.js
+++ starter-files/script.js
@@ -7,3 +7,44 @@ STEPS
 - Output results to the UI (success and error)
 - Adjust UI states accordingly
 */
+const submitButton = document.querySelector("#submit");
+const input = document.querySelector("#input");
+const errorSpan = document.querySelector("#error");
+const resultsContainer = document.querySelector("#results");
+
+const endpoint = "https://en.wikipedia.org/w/api.php?";
+const params = {
+  origin: "*",
+  format: "json",
+  action: "query",
+  prop: "extracts",
+  exchars: 250,
+  exintro: "exintro",
+  explaintext: true,
+  generator: "search",
+  gsrlimit: 20,
+};
+
+const disableUi = () => {
+  input.disabled = true;
+  submitButton.disabled = true;
+};
+
+const enableUi = () => {
+  input.disabled = false;
+  submitButton.disabled = false;
+};
+
+const clearPreviousResults = () => {
+  resultsContainer.innerHTML = "";
+  errorSpan.innerHTML = "";
+};
+
+const isInputEmpty = (input) => {
+  if (!input || input === "") return true;
+  return false;
+};
+
+const showError = (error) => {
+  errorSpan.innerHTML = `${error}`;
+};
